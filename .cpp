ea15832ea06af94c2ed6e81f67eb5b1ab4078532
Dhika270 <andikasetiawan05ds@gmail.com>
2025-06-12 18:37:34 +0700

    progres 11

--- .cpp
+++ .cpp
@@ -103,6 +103,18 @@ public:
                     cout << queue_array[FRONT_position] << "  ";
                     FRONT_position++;
                 }
+                cout << endl;
             }
         }
+};
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        
+    }
 }

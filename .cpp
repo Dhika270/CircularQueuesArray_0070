4e8eb714aeb9d54d65e87484ce91bb2015ac645a
Dhika270 <andikasetiawan05ds@gmail.com>
2025-06-12 18:33:43 +0700

    progres 10

--- .cpp
+++ .cpp
@@ -91,6 +91,17 @@ public:
                 while (FRONT_position <= REAR_position)
                 {
                     cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+                cout << endl;
+            }
+            else
+            {
+                // Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+                while (FRONT_position <= max -1)
+                {
+                    cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
                 }
             }
         }

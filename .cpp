cbd8497e9067f0f21dca087e9d500aab4500231a
Dhika270 <andikasetiawan05ds@gmail.com>
2025-06-12 18:42:06 +0700

    progres 12

--- .cpp
+++ .cpp
@@ -115,6 +115,15 @@ int main()
 
     while (true)
     {
-        
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete opreation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+        }
     }
 }
